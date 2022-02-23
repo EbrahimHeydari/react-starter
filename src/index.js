b3755805b399ca6a5fa22003ecfc02c33a59bdbc
Ebrahim780 <akbrly42@gmail.com>
2022-02-23 10:49:28 +0330

    remove redux

--- src/index.js
+++ src/index.js
@@ -12,26 +12,3 @@ ReactDOM.render(
 );
 
 reportWebVitals();
-
-
-
-// import React from 'react';
-// import reactDom from 'react-dom';
-// import { createStore } from 'redux';
-// import Reducer from './Practice/Redux/reducers'
-// import Counter from './Practice/Redux/components/counter';
-// import './index.css';
-
-// const store = createStore(Reducer)
-// const rootElement = document.getElementById('root');
-
-// const render = () => reactDom.render(
-//   <Counter
-//     value={store.getState()}
-//     onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
-//     onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
-//   />, rootElement
-// )
-
-// render()
-// store.subscribe(render)
