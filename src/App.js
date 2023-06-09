fcb964a0b9ce4523de7c1bfd84aa60bc9bc2ea61
Ebrahim <akbrly42@gmail.com>
2023-06-09 15:07:10 +0430

    add pwa feature

--- src/App.js
+++ src/App.js
@@ -107,6 +107,37 @@
 
 // export default App
 
+// UseImperativeHandle Example
+// import React, { forwardRef, useImperativeHandle, useRef, useState } from 'react'
+
+// // Parent Component
+// const App = () => {
+// 	const ref = useRef()
+
+// 	return (
+// 		<div>
+// 			<Button ref={ref} />
+// 			<button onClick={() => ref.current.increment()}>another button</button>
+// 		</div>
+// 	)
+// }
+
+// export default App
+
+// // Child Component
+// const Button = forwardRef((props, ref) => {
+// 	useImperativeHandle(ref, () => ({ increment }))
+
+// 	const [count, setCount] = useState(0)
+// 	const increment = () => setCount(count + 1)
+// 	return (
+// 		<div>
+// 			<button onClick={increment}>click</button>
+// 			<h2>Count: {count}</h2>
+// 		</div>
+// 	)
+// })
+
 // Favorites (Reducer)
 import React from 'react'
 import FavoriteTheme from './Project/Favorites/contexts/theme'
--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import App from './App'
 import './index.css'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
+serviceWorkerRegistration.register()
